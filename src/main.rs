c4dd3918a4b2625efd72f26b58fbfbf7eeb0beb8
GauravN0910 <root@Gaurav>
2025-07-23 16:18:35 +0530

    Trivial Test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 use core::panic::PanicInfo;
 
@@ -10,11 +21,23 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[test_case]
+fn sample_test() {
+    print!("Sample Test = ");
+    assert_eq!(1, 1);
+    println!("Mudinch");
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     use core::fmt::Write;
     vga_buffer::WRITER.lock().write_str("Welcome to Not-Linux\n").unwrap();
     write!(vga_buffer::WRITER.lock(), "Printed using Rust FMT\n").unwrap();
     println!("Printed using println macro");
+
+    #[cfg(test)]
+    test_main();
     loop {}
 }
+
+
